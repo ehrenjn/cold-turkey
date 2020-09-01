204b184f4d325e9db2c5fb38571734cd7d08de3c
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-09-01 16:29:37 -0400

    moved comments to header file

--- cold_turkey.c
+++ cold_turkey.c
@@ -1,9 +1,8 @@
 /*
 How to nicely make double and double version?
+    cold_turkey.c and cold_turkey_float.c
 Should I move documentation comments to header file instead?
     YES
-If header file only declares "public" functions that works right?
-should probably make helper functions static
 */
 
 #include <stdint.h>
@@ -138,58 +137,22 @@ int radix_2_cooley_tukey(double* freqs_real, double* freqs_imaginary, uint32_t t
 }
 
 
-/*
-calculates DFT of signal and stores it in freqs
-may be faster than fft for short signals
 
-signal_real: real component of input signal
-signal_imaginary: imaginary component of input signal (usually all 0s)
-freqs_real: real component of output frequencies
-freqs_imaginary: imaginary component of output frequencies
-length: the length of all the arrays (they must all be the same length)
-*/
 void dft(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length) {
     dft_loops(signal_real, signal_imaginary, freqs_real, freqs_imaginary, length, -1, 1);
 }
 
 
-/*
-calculates inverse DFT of freqs and stores it in signal
-may be faster than ifft for short inputs
-
-freqs_real: real component of input frequencies
-freqs_imaginary: imaginary component of input frequencies
-signal_real: real component of output signal
-signal_imaginary: imaginary component of output signal
-length: the length of all the arrays (they must all be the same length)
-*/
 void idft(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length) {
-    dft_loops(signal_real, signal_imaginary, freqs_real, freqs_imaginary, length, 1, length);
+    dft_loops(freqs_real, freqs_imaginary, signal_real, signal_imaginary, length, 1, length);
 }
 
 
-/*
-an in-place FFT
-length must be a power of 2 and less than 2^32
-returns -1 if unsuccessful
-
-signal_real: real component of input signal
-signal_imaginary: imaginary component of input signal (usually all 0s)
-length: the length of the signal
-*/
 int fft(double* signal_real, double* signal_imaginary, uint32_t length) {
     return radix_2_cooley_tukey(signal_real, signal_imaginary, length, -1); // -1 for counterclockwise
 }
 
-/*
-an in-place inverse FFT
-length must be a power of 2 and less than 2^32
-returns -1 if unsuccessful
 
-freqs_real: real component of input frequencies
-freqs_imaginary: imaginary component of input frequencies
-length: the length of the frequency arrays
-*/
 int ifft(double* freqs_real, double* freqs_imaginary, uint32_t length) {
     int success = radix_2_cooley_tukey(freqs_real, freqs_imaginary, length, 1); // 1 for clockwise
     if (success != -1) {
@@ -202,17 +165,6 @@ int ifft(double* freqs_real, double* freqs_imaginary, uint32_t length) {
 }
 
 
-/*
-calculates FFT of signal and stores it in freqs
-length must be a power of 2 and less than 2^32
-returns -1 if unsuccessful
-
-signal_real: real component of input signal
-signal_imaginary: imaginary component of input signal (usually all 0s)
-freqs_real: real component of output frequencies
-freqs_imaginary: imaginary component of output frequencies
-length: the length of all the arrays (they must all be the same length)
-*/
 int fft_out_of_place(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length) {
     // copy signal into frequency array and then do in place fft
     for (uint32_t sample = 0; sample < length; sample++) {
@@ -222,17 +174,7 @@ int fft_out_of_place(double* signal_real, double* signal_imaginary, double* freq
     return fft(freqs_real, freqs_imaginary, length);
 }
 
-/*
-calculates inverse FFT of freqs and stores it in signal
-length must be a power of 2 and less than 2^32
-returns -1 if unsuccessful
 
-freqs_real: real component of input frequencies
-freqs_imaginary: imaginary component of input frequencies
-signal_real: real component of output signal
-signal_imaginary: imaginary component of output signal
-length: the length of all the arrays (they must all be the same length)
-*/
 int ifft_out_of_place(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length) {
     // copy frequencies into signal array and then do in place ifft
     for (uint32_t bin = 0; bin < length; bin++) {
---
+++ cold_turkey.h
@@ -0,0 +1,84 @@
+
+#ifndef COLD_TURKEY_INCLUDED
+#define COLD_TURKEY_INCLUDED
+
+
+#include <stdint.h>
+
+
+/*
+calculates DFT of signal and stores it in freqs
+may be faster than fft for short signals
+
+signal_real: real component of input signal
+signal_imaginary: imaginary component of input signal (usually all 0s)
+freqs_real: real component of output frequencies
+freqs_imaginary: imaginary component of output frequencies
+length: the length of all the arrays (they must all be the same length)
+*/
+void dft(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length);
+
+/*
+calculates inverse DFT of freqs and stores it in signal
+may be faster than ifft for short inputs
+
+freqs_real: real component of input frequencies
+freqs_imaginary: imaginary component of input frequencies
+signal_real: real component of output signal
+signal_imaginary: imaginary component of output signal
+length: the length of all the arrays (they must all be the same length)
+*/
+void idft(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length);
+
+
+/*
+an in-place FFT
+length must be a power of 2 and less than 2^32
+returns -1 if unsuccessful
+
+signal_real: real component of input signal
+signal_imaginary: imaginary component of input signal (usually all 0s)
+length: the length of the signal
+*/
+int fft(double* signal_real, double* signal_imaginary, uint32_t length);
+
+/*
+an in-place inverse FFT
+length must be a power of 2 and less than 2^32
+returns -1 if unsuccessful
+
+freqs_real: real component of input frequencies
+freqs_imaginary: imaginary component of input frequencies
+length: the length of the frequency arrays
+*/
+int ifft(double* freqs_real, double* freqs_imaginary, uint32_t length);
+
+
+/*
+calculates FFT of signal and stores it in freqs
+length must be a power of 2 and less than 2^32
+returns -1 if unsuccessful
+
+signal_real: real component of input signal
+signal_imaginary: imaginary component of input signal (usually all 0s)
+freqs_real: real component of output frequencies
+freqs_imaginary: imaginary component of output frequencies
+length: the length of all the arrays (they must all be the same length)
+*/
+int fft_out_of_place(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length);
+
+/*
+calculates inverse FFT of freqs and stores it in signal
+length must be a power of 2 and less than 2^32
+returns -1 if unsuccessful
+
+freqs_real: real component of input frequencies
+freqs_imaginary: imaginary component of input frequencies
+signal_real: real component of output signal
+signal_imaginary: imaginary component of output signal
+length: the length of all the arrays (they must all be the same length)
+*/
+int ifft_out_of_place(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length);
+
+
+#endif
