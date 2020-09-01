0366dd1c2470b8c1be6d0a24b077365552d772bc
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-09-01 14:52:44 -0400

    changed floats to doubles

--- cold_turkey.c
+++ cold_turkey.c
@@ -1,6 +1,7 @@
 /*
-How to nicely make double and float version?
+How to nicely make double and double version?
 Should I move documentation comments to header file instead?
+    YES
 If header file only declares "public" functions that works right?
 should probably make helper functions static
 */
@@ -21,18 +22,18 @@ should probably make helper functions static
     ((a_real) * (b_imaginary) + (a_imaginary) * (b_real))
 
 
-void dft_loops(float* signal_real, float* signal_imaginary, float* freqs_real, float* freqs_imaginary, 
+void dft_loops(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, 
     uint32_t length, int direction, uint32_t normalization_factor) 
 {
     for (uint32_t f = 0; f < length; f++) {
-        float real_sum = 0;
-        float imaginary_sum = 0;
+        double real_sum = 0;
+        double imaginary_sum = 0;
         for (uint32_t t = 0; t < length; t++) {
             
             // evaluate real and imaginary components of e^(-2*i*pi*f*t/length) individually using euler's formula
-            float exponent = direction * 2 * PI * f * t / length;
-            float exponent_real = cos(exponent);
-            float exponent_imaginary = sin(exponent);
+            double exponent = direction * 2 * PI * f * t / length;
+            double exponent_real = cos(exponent);
+            double exponent_imaginary = sin(exponent);
 
             // binomial expansion for multiplying two complex numbers
             real_sum += complex_multiply_real(signal_real[t], signal_imaginary[t], exponent_real, exponent_imaginary);
@@ -45,13 +46,13 @@ void dft_loops(float* signal_real, float* signal_imaginary, float* freqs_real, f
 
 
 // edited version of bit reverse function from the algorithm-archive: https://github.com/algorithm-archivists/algorithm-archive/blob/master/contents/cooley_tukey/code/c%2B%2B/fft.cpp
-void sort_in_bit_reversed_order(float* array, uint32_t length) {
+void sort_in_bit_reversed_order(double* array, uint32_t length) {
     uint32_t num_bits = (uint32_t)(log2(length)); // number of bits needed to represent any location in array
     uint32_t num_swaps = (uint32_t)(ceil(log2(num_bits))); // this algorithm works by repeatedly shifting around smaller and smaller groups of bits, we COULD do all the bit group swaps for all input sizes but it's a waste of time if we only need to reverse like 5 bits
     uint32_t num_trailing_zeros = (uint32_t)(pow(2, num_swaps) - num_bits); // number of trailing 0's that we need to get rid of when the bit string is reversed
 
     for (int index = 0; index < length; index++) {
-        
+
         uint32_t rev_index = index;                                                                 // 00000012 3456789A (example 10 bit index)
         switch (num_swaps) { // switch is just for efficiency: do as few bit swaps as possible
             case 5: // start here if num_bits > 16
@@ -68,7 +69,7 @@ void sort_in_bit_reversed_order(float* array, uint32_t length) {
         rev_index = rev_index >> num_trailing_zeros;                                                // 000000A9 87654321 (shift to remove trailing 0s)
         
         if (rev_index > index) { // make sure we don't swap the same 2 elements twice
-            float value_at_index = array[index];
+            double value_at_index = array[index];
             array[index] = array[rev_index];
             array[rev_index] = value_at_index;
         }
@@ -77,7 +78,7 @@ void sort_in_bit_reversed_order(float* array, uint32_t length) {
 
 
 // an iterative, decimation in time, radix 2 cooley tukey
-int radix_2_cooley_tukey(float* freqs_real, float* freqs_imaginary, uint32_t total_length, int direction) {
+int radix_2_cooley_tukey(double* freqs_real, double* freqs_imaginary, uint32_t total_length, int direction) {
 
     // throw error if direction or total_length is wrong
     if (((total_length & (total_length - 1)) != 0) || // check if total_length is a power of 2
@@ -92,8 +93,8 @@ int radix_2_cooley_tukey(float* freqs_real, float* freqs_imaginary, uint32_t tot
     // do length 2 DFTs first (seperate loop because skipping twiddle factors)
     for (uint32_t even_index = 0; even_index < total_length; even_index += 2) {
         uint32_t odd_index = even_index + 1;
-        float even_value_real = freqs_real[even_index];
-        float even_value_imaginary = freqs_imaginary[even_index];
+        double even_value_real = freqs_real[even_index];
+        double even_value_imaginary = freqs_imaginary[even_index];
 
         freqs_real[even_index] = even_value_real + freqs_real[odd_index];
         freqs_real[odd_index] = even_value_real - freqs_real[odd_index];
@@ -111,18 +112,18 @@ int radix_2_cooley_tukey(float* freqs_real, float* freqs_imaginary, uint32_t tot
 
                 uint32_t even_index = dft_start + frequency_num;
                 uint32_t odd_index = even_index + half_dft_length;
-                float even_value_real = freqs_real[even_index];
-                float even_value_imaginary = freqs_imaginary[even_index];
+                double even_value_real = freqs_real[even_index];
+                double even_value_imaginary = freqs_imaginary[even_index];
                 
                 // evaluate real and imaginary components of twiddle factor individually using euler's formula
-                float exponent = direction * 2 * PI * frequency_num / dft_length;
-                float twiddle_real = cos(exponent);
-                float twiddle_imaginary = sin(exponent);
+                double exponent = direction * 2 * PI * frequency_num / dft_length;
+                double twiddle_real = cos(exponent);
+                double twiddle_imaginary = sin(exponent);
 
                 // multiply twiddle factor by odd value
-                float odd_times_twiddle_real = complex_multiply_real(
+                double odd_times_twiddle_real = complex_multiply_real(
                     freqs_real[odd_index], freqs_imaginary[odd_index], twiddle_real, twiddle_imaginary);
-                float odd_times_twiddle_imaginary = complex_multiply_imaginary(
+                double odd_times_twiddle_imaginary = complex_multiply_imaginary(
                     freqs_real[odd_index], freqs_imaginary[odd_index], twiddle_real, twiddle_imaginary);
 
                 freqs_real[even_index] = even_value_real + odd_times_twiddle_real;
@@ -147,7 +148,7 @@ freqs_real: real component of output frequencies
 freqs_imaginary: imaginary component of output frequencies
 length: the length of all the arrays (they must all be the same length)
 */
-void dft(float* signal_real, float* signal_imaginary, float* freqs_real, float* freqs_imaginary, uint32_t length) {
+void dft(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length) {
     dft_loops(signal_real, signal_imaginary, freqs_real, freqs_imaginary, length, -1, 1);
 }
 
@@ -162,7 +163,7 @@ signal_real: real component of output signal
 signal_imaginary: imaginary component of output signal
 length: the length of all the arrays (they must all be the same length)
 */
-void idft(float* freqs_real, float* freqs_imaginary, float* signal_real, float* signal_imaginary, uint32_t length) {
+void idft(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length) {
     dft_loops(signal_real, signal_imaginary, freqs_real, freqs_imaginary, length, 1, length);
 }
 
@@ -176,7 +177,7 @@ signal_real: real component of input signal
 signal_imaginary: imaginary component of input signal (usually all 0s)
 length: the length of the signal
 */
-int fft(float* signal_real, float* signal_imaginary, uint32_t length) {
+int fft(double* signal_real, double* signal_imaginary, uint32_t length) {
     return radix_2_cooley_tukey(signal_real, signal_imaginary, length, -1); // -1 for counterclockwise
 }
 
@@ -189,7 +190,7 @@ freqs_real: real component of input frequencies
 freqs_imaginary: imaginary component of input frequencies
 length: the length of the frequency arrays
 */
-int ifft(float* freqs_real, float* freqs_imaginary, uint32_t length) {
+int ifft(double* freqs_real, double* freqs_imaginary, uint32_t length) {
     int success = radix_2_cooley_tukey(freqs_real, freqs_imaginary, length, 1); // 1 for clockwise
     if (success != -1) {
         for (uint32_t sample = 0; sample < length; sample++) {
@@ -212,7 +213,7 @@ freqs_real: real component of output frequencies
 freqs_imaginary: imaginary component of output frequencies
 length: the length of all the arrays (they must all be the same length)
 */
-int fft_out_of_place(float* signal_real, float* signal_imaginary, float* freqs_real, float* freqs_imaginary, uint32_t length) {
+int fft_out_of_place(double* signal_real, double* signal_imaginary, double* freqs_real, double* freqs_imaginary, uint32_t length) {
     // copy signal into frequency array and then do in place fft
     for (uint32_t sample = 0; sample < length; sample++) {
         freqs_real[sample] = signal_real[sample];
@@ -232,7 +233,7 @@ signal_real: real component of output signal
 signal_imaginary: imaginary component of output signal
 length: the length of all the arrays (they must all be the same length)
 */
-int ifft_out_of_place(float* freqs_real, float* freqs_imaginary, float* signal_real, float* signal_imaginary, uint32_t length) {
+int ifft_out_of_place(double* freqs_real, double* freqs_imaginary, double* signal_real, double* signal_imaginary, uint32_t length) {
     // copy frequencies into signal array and then do in place ifft
     for (uint32_t bin = 0; bin < length; bin++) {
         signal_real[bin] = freqs_real[bin];
